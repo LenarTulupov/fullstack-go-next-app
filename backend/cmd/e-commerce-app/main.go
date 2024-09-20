8f0792f776e0a7859f936a0ab8cc036de0aacb95
Lenar <e6naa7981rrshift934a@gmail.com>
2024-09-21 02:32:35 +0400

    check

--- backend/cmd/e-commerce-app/main.go
+++ backend/cmd/e-commerce-app/main.go
@@ -16,7 +16,6 @@ func CORSMiddleware() gin.HandlerFunc {
         c.Header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
         c.Header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept")
         
-        // Allow preflight requests
         if c.Request.Method == http.MethodOptions {
             c.AbortWithStatus(http.StatusNoContent)
             return
@@ -27,15 +26,12 @@ func CORSMiddleware() gin.HandlerFunc {
 }
 
 func main() {
-    // Load configuration
     config.LoadConfig()
 
-    // Initialize router
     r := router.SetupRouter()
 
     r.Use(CORSMiddleware())
 
-    // Start server
     port := ":8080"
     fmt.Printf("Server is running on port %s\n", port)
     if err := r.Run(port); err != nil {
