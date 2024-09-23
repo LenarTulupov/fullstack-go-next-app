e49e44a2a211d1642fd414eddd90dd83c0d9df89
Lenar <e6naa7981rrshift934a@gmail.com>
2024-09-23 23:45:35 +0400

    refactor: installed different cors package

--- backend/internal/middleware/cors.go
+++ backend/internal/middleware/cors.go
@@ -1,23 +1,18 @@
 package middleware
 
 import (
-    "net/http"
-
-    "github.com/gin-gonic/gin"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"time"
 )
 
 func CORSMiddleware() gin.HandlerFunc {
-    return func(c *gin.Context) {
-        c.Header("Access-Control-Allow-Origin", "*")
-        c.Header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
-        c.Header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, Authorization")
-        c.Header("Access-Control-Allow-Credentials", "true")
-
-        if c.Request.Method == http.MethodOptions {
-            c.AbortWithStatus(http.StatusNoContent)
-            return
-        }
-
-        c.Next()
-    }
+	return cors.New(cors.Config{
+			AllowOrigins:     []string{"https://fullstack-go-next-app-4.onrender.com"}, 
+			AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+			AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+			ExposeHeaders:    []string{"Content-Length"},
+			AllowCredentials: true,
+			MaxAge:           12 * time.Hour, 
+	})
 }
