5b45814b0047db84b25f786bcfe42f567793bbba
Lenar <e6naa7981rrshift934a@gmail.com>
2024-09-24 00:09:06 +0400

    new cors test

--- backend/internal/middleware/cors.go
+++ backend/internal/middleware/cors.go
@@ -1,14 +1,18 @@
 package middleware
 
 import (
+	"time"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	"time"
 )
 
 func CORSMiddleware() gin.HandlerFunc {
 	return cors.New(cors.Config{
-			AllowOrigins:     []string{"https://fullstack-go-next-app-4.onrender.com"}, 
+			AllowOrigins:     []string{
+				"https://fullstack-go-next-app-4.onrender.com",
+    		"https://fullstack-go-next-app.onrender.com",
+			}, 
 			AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 			AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
 			ExposeHeaders:    []string{"Content-Length"},
