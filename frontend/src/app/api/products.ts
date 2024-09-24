d7ab72adbbc6f8e0c7825623e24194b4a76ca4b4
Lenar <e6naa7981rrshift934a@gmail.com>
2024-09-25 01:19:32 +0400

    simple fetch cors func

--- frontend/src/app/api/products.ts
+++ frontend/src/app/api/products.ts
@@ -1,16 +1,10 @@
-import { NextResponse } from 'next/server';
+export async function GET(request: Request) {
+  const res = await fetch('https://fullstack-go-next-app.onrender.com/products');
+  const data = await res.json();
 
-export async function GET(req: Request) {
-    try {
-        const response = await fetch('https://fullstack-go-next-app.onrender.com/products');
-        const data = await response.json();
-
-        if (response.ok) {
-            return NextResponse.json(data);
-        } else {
-            return NextResponse.json({ message: 'Failed to fetch products' }, { status: response.status });
-        }
-    } catch (error) {
-        return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 });
+  return new Response(JSON.stringify(data), {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
     }
+  });
 }
