1b7168e112732ce9e363cbc3e2bc34d97175cfd1
Lenar <e6naa7981rrshift934a@gmail.com>
2024-10-23 19:34:31 +0400

    fix: pagination component types

--- frontend/src/components/pagination/pagination.tsx
+++ frontend/src/components/pagination/pagination.tsx
@@ -1,9 +1,9 @@
 import { IProduct } from "@/types/product.interface"
 import { useState } from "react";
-import Card from "../card/card";
+// import Card from "../card/card";
 import styles from './pagination.module.scss'
-import { useDispatch, useSelector } from "react-redux";
-import { addFavorite, removeFavorite, selectFavoriteProducts } from "@/store/favorite-button/favoriteButtonSlice";
+// import { useDispatch, useSelector } from "react-redux";
+// import { addFavorite, removeFavorite, selectFavoriteProducts } from "@/store/favorite-button/favoriteButtonSlice";
 
 interface IPagination {
   products: IProduct[];
@@ -11,50 +11,43 @@ interface IPagination {
 
 export default function Pagination({ products }: IPagination) {
   const [currentPage, setCurrentPage] = useState<number>(1);
-  const dispatch = useDispatch();
-  const favoriteProducts = useSelector(selectFavoriteProducts);
+  // const dispatch = useDispatch();
+  // const favoriteProducts = useSelector(selectFavoriteProducts);
 
-  const handleAddToFavorite = (product: IProduct) => {
-    const isFavorite = favoriteProducts.some(favProduct => favProduct.id === product.id);
+  // const handleAddToFavorite = (product: IProduct) => {
+  //   const isFavorite = favoriteProducts.some(favProduct => favProduct.id === product.id);
 
-    if (isFavorite) {
-      dispatch(removeFavorite(product.id));
-    } else {
-      dispatch(addFavorite(product));
-    }
-  };
+  //   if (isFavorite) {
+  //     dispatch(removeFavorite(product.id));
+  //   } else {
+  //     dispatch(addFavorite(product));
+  //   }
+  // };
 
   const ITEMS_PER_PAGE = 25;
   const totalPages = Math.ceil(products.length / ITEMS_PER_PAGE);
-  const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
-  const endIndex = startIndex + ITEMS_PER_PAGE;
+  // const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
+  // const endIndex = startIndex + ITEMS_PER_PAGE;
 
-  const currentProducts = products.slice(startIndex, endIndex);
+  // const currentProducts = products.slice(startIndex, endIndex);
 
   return (
     <div>
       <div className={styles.pagination__products}>
 
-        {currentProducts.map((product) => {
+        {/* {currentProducts.map((product) => {
           const isFavorite = favoriteProducts.some(
             favProduct => favProduct.id === product.id);
           console.log(isFavorite)
           return (
             <Card
               key={product.id}
-              id={product.id}
-              title={product.title}
-              images={product.images}
-              alt={product.title}
-              price_new={product.price_new}
-              price_old={product.price_old}
-              sizes={product.sizes}
-              color={product.color}
+              product={product}
               handleFavorite={() => handleAddToFavorite(product)}
               description={product.description}
             />
           )
-        })}
+        })} */}
       </div>
 
       <div className={styles.pagination__buttons}>
