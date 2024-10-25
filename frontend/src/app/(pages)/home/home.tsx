8a25088282bf4ad5baa70fca919d46384323192b
Lenar <e6naa7981rrshift934a@gmail.com>
2024-10-26 00:09:45 +0400

    fix: layout loader and home loader with portals

--- frontend/src/app/(pages)/home/home.tsx
+++ frontend/src/app/(pages)/home/home.tsx
@@ -5,9 +5,9 @@ import { bannerImagesLarge } from '@/constants/banners-images'
 import Image from 'next/image'
 import Link from 'next/link'
 import styles from './home.module.scss'
-import Head from 'next/head'
-import BannerSkeleton from '@/components/ui/banner-skeleton/banner-skeleton'
 import { useEffect, useState } from 'react'
+import Loader from '@/components/ui/loader/loader'
+import { createPortal } from 'react-dom'
 
 export default function Home() {
   const [isLoading, setIsLoading] = useState<boolean>(true);
@@ -17,49 +17,40 @@ export default function Home() {
   }, [])
 
   return (
-    <>
-      <Head>
-        <link
-          rel="preload"
-          href={bannerImagesLarge[0]}
-          as="image"
-          type="image/jpeg"
-        />
-      </Head>
-      <div className={styles.home}>
-        <div className={styles['carousel-wrapper']}>
-          {isLoading ? (
-            <BannerSkeleton />
-          ) : (
-            <Carousel
-              dots={false}
-              infinite={true}
-              speed={2000}
-              autoplay
-              autoplaySpeed={10000}
-              effect='fade'
-            >
-              {bannerImagesLarge.map((banner, index) => (
-                <Link
-                  key={index}
-                  href='/category/trends'
-                  className={styles['image-link']}
-                >
-                  <div className={styles['image-wrapper']}>
-                    <Image
-                      src={banner}
-                      alt="banner"
-                      fill
-                      objectFit='cover'
-                      priority
-                    />
-                  </div>
-                </Link>
-              ))}
-            </Carousel>
-          )}
-        </div>
+    <div className={styles.home}>
+      <div className={styles['carousel-wrapper']}>
+        {isLoading ? createPortal(
+          <Loader className={styles.loader}/>,
+          document.body
+        ) : (
+          <Carousel
+            dots={false}
+            infinite={true}
+            speed={2000}
+            autoplay
+            autoplaySpeed={10000}
+            effect='fade'
+          >
+            {bannerImagesLarge.map((banner, index) => (
+              <Link
+                key={index}
+                href='/category/trends'
+                className={styles['image-link']}
+              >
+                <div className={styles['image-wrapper']}>
+                  <Image
+                    src={banner}
+                    alt="banner"
+                    fill
+                    objectFit='cover'
+                    priority
+                  />
+                </div>
+              </Link>
+            ))}
+          </Carousel>
+        )}
       </div>
-    </>
+    </div>
   )
 }
--- frontend/src/components/ui/loader/loader.tsx
+++ frontend/src/components/ui/loader/loader.tsx
@@ -1,9 +1,12 @@
 import styles from './loader.module.scss';
 
+interface ILoader {
+  className?: string;
+}
 
-export default function Loader() {
+export default function Loader({ className }: ILoader) {
   return (
-    <div className={styles.loader}>
+    <div className={`${styles.loader} ${className || ''}`}>
       <div className={styles.loader__animation} />
       <div className={styles.loader__text}>Loading...</div>
     </div>
