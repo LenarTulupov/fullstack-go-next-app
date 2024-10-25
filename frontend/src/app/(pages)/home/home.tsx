5e2509be1344e672bf75c00a6736215dbc0f9737
LenarTulupov <e6naa7981rrshift934a@gmail.com>
2024-10-25 22:16:28 +0400

    fix: banner skeleton styles

--- frontend/src/app/(pages)/home/home.tsx
+++ frontend/src/app/(pages)/home/home.tsx
@@ -1,14 +1,24 @@
+'use client'
+
 import { Carousel } from 'antd'
 import { bannerImagesLarge } from '@/constants/banners-images'
 import Image from 'next/image'
 import Link from 'next/link'
 import styles from './home.module.scss'
 import Head from 'next/head'
+import BannerSkeleton from '@/components/ui/banner-skeleton/banner-skeleton'
+import { useEffect, useState } from 'react'
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    setIsLoading(false);
+  }, [])
+
   return (
     <>
-    <Head>
+      <Head>
         <link
           rel="preload"
           href={bannerImagesLarge[0]}
@@ -18,32 +28,36 @@ export default function Home() {
       </Head>
       <div className={styles.home}>
         <div className={styles['carousel-wrapper']}>
-          <Carousel
-            dots={false}
-            infinite={true}
-            speed={2000}
-            autoplay
-            autoplaySpeed={10000}
-            effect='fade'
-          >
-            {bannerImagesLarge.map((banner, index) => (
-              <Link
-                key={index}
-                href='/category/trends'
-                className={styles['image-link']}
-              >
-                <div className={styles['image-wrapper']}>
-                  <Image
-                    src={banner}
-                    alt="banner"
-                    fill
-                    objectFit='cover'
-                    priority
-                  />
-                </div>
-              </Link>
-            ))}
-          </Carousel>
+          {isLoading ? (
+            <BannerSkeleton />
+          ) : (
+            <Carousel
+              dots={false}
+              infinite={true}
+              speed={2000}
+              autoplay
+              autoplaySpeed={10000}
+              effect='fade'
+            >
+              {bannerImagesLarge.map((banner, index) => (
+                <Link
+                  key={index}
+                  href='/category/trends'
+                  className={styles['image-link']}
+                >
+                  <div className={styles['image-wrapper']}>
+                    <Image
+                      src={banner}
+                      alt="banner"
+                      fill
+                      objectFit='cover'
+                      priority
+                    />
+                  </div>
+                </Link>
+              ))}
+            </Carousel>
+          )}
         </div>
       </div>
     </>
---
+++ frontend/src/components/ui/banner-skeleton/banner-skeleton.tsx
@@ -0,0 +1,8 @@
+import styles from './banner-skeleton.module.scss';
+
+
+export default function BannerSkeleton() {
+  return (
+    <div className={styles['banner-skeleton']}/>
+  )
+};
