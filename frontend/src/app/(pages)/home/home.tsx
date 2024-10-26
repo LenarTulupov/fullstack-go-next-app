a229ee02f1c03c4a5c974732f552b1f4b97c05dd
Lenar <e6naa7981rrshift934a@gmail.com>
2024-10-27 00:05:45 +0400

    fix: test5 banner is loaded log

--- frontend/src/app/(pages)/home/home.tsx
+++ frontend/src/app/(pages)/home/home.tsx
@@ -5,63 +5,42 @@ import { bannerImagesLarge } from '@/constants/banners-images'
 import Image from 'next/image'
 import Link from 'next/link'
 import styles from './home.module.scss'
-import { useEffect, useState } from 'react'
-import Loader from '@/components/ui/loader/loader'
 
 export default function Home() {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const loadImages = async () => {
-      const imageLoaders = bannerImagesLarge.map(
-        (src) =>
-          new Promise((resolve, reject) => {
-            const img = new window.Image()
-            img.src = src
-            img.onload = resolve
-            img.onerror = reject
-          })
-      )
-      await Promise.all(imageLoaders)
-      setLoading(false) 
-    }
-
-    loadImages()
-  }, [])
+  const handleImageLoad = (url: string) => {
+    console.log(`Image loaded: ${url}`)
+  }
   return (
     <div className={styles.home}>
       <div className={styles['carousel-wrapper']}>
-      {loading ? (
-          <Loader/>
-        ) : (
-          <Carousel
-            dots={false}
-            infinite={true}
-            speed={2000}
-            autoplay
-            autoplaySpeed={10000}
-            effect="fade"
-          >
-            {bannerImagesLarge.map((banner, index) => (
-              <Link
-                key={index}
-                href="/category/trends"
-                className={styles['image-link']}
-              >
-                <div className={styles['image-wrapper']}>
-                  <Image
-                    src={banner}
-                    alt="banner"
-                    width={0}
-                    height={0}
-                    layout="responsive"
-                    priority={index === 0}
-                  />
-                </div>
-              </Link>
-            ))}
-          </Carousel>
-        )}
+        <Carousel
+          dots={false}
+          infinite={true}
+          speed={2000}
+          autoplay
+          autoplaySpeed={10000}
+          effect="fade"
+        >
+          {bannerImagesLarge.map((banner, index) => (
+            <Link
+              key={index}
+              href="/category/trends"
+              className={styles['image-link']}
+            >
+              <div className={styles['image-wrapper']}>
+                <Image
+                  src={banner}
+                  alt="banner"
+                  width={0}
+                  height={0}
+                  layout="responsive"
+                  priority={index === 0}
+                  onLoadingComplete={() => handleImageLoad(banner)}
+                />
+              </div>
+            </Link>
+          ))}
+        </Carousel>
       </div>
     </div>
   )
