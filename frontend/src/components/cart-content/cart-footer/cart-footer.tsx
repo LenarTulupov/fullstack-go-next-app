a822a26e0273bf7ff1e90ff8f587562992f8894d
Lenar <e6naa7981rrshift934a@gmail.com>
2024-11-04 14:28:38 +0400

    refactor: cart total price and subtotal with format currency

--- frontend/src/components/cart-content/cart-footer/cart-footer.tsx
+++ frontend/src/components/cart-content/cart-footer/cart-footer.tsx
@@ -3,6 +3,7 @@ import styles from './cart-footer.module.scss';
 import Button from '@/components/ui/button/button';
 import Link from 'next/link';
 import CartService from '@/services/cart.service';
+import Price from '@/components/ui/price/price';
 
 export default function CartFooter() {
   const { totalPrice, discount } = CartService();
@@ -10,14 +11,14 @@ export default function CartFooter() {
     <div className={styles['cart-footer']}>
       <div className={styles['cart-footer__discount']}>
         <Title weight='bold'>Discount</Title>
-        <Title weight='bold'>{discount}</Title>
+        <Price price={discount}/>
       </div>
       <div className={styles['cart-footer__subtotal']}>
         <div>
           <Title weight='bold'>Subtotal (excluding delivery)</Title>
           <Title>*All taxes are included in product prices</Title>
         </div>
-        <Title weight='bold'>{totalPrice}</Title>
+        <Price price={totalPrice}/>
       </div>
       <div className={styles['cart-footer__buttons']}>
         <Link
--- frontend/src/components/ui/price/price.tsx
+++ frontend/src/components/ui/price/price.tsx
@@ -2,15 +2,22 @@ import formatCurrency from '@/utils/formatCurrency';
 import styles from './price.module.scss'
 
 interface IPrice {
-  price: string;
+  price: string | number;
   className?: string;
   currency?: 'EUR' | 'USD';
   old?: boolean;
 }
 
-export default function Price({ price, className, currency = 'EUR', old = false }: IPrice ) {
+export default function Price({ 
+  price, 
+  className, 
+  currency = 'EUR', 
+  old = false }: IPrice) {
   return (
-    <div className={`${styles.price} ${old ? styles.old : ''} ${className || ''}`}>
+    <div className={`
+      ${styles.price} ${old ? styles.old : ''} 
+      ${className || ''}
+    `}>
       {formatCurrency(
         { price, currency }
       )}
