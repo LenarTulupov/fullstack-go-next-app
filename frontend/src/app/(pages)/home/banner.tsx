365f749610d192a3e312a248b5d681eaa8215ed8
Lenar <e6naa7981rrshift934a@gmail.com>
2024-12-01 01:12:21 +0400

    feature: mobile banners

--- frontend/src/app/(pages)/home/banner.tsx
+++ frontend/src/app/(pages)/home/banner.tsx
@@ -1,39 +1,52 @@
 import { Carousel } from 'antd'
-import { bannerImagesLarge } from '@/constants/banners-images'
+import { bannerImagesLarge, bannerImagesMobile } from '@/constants/banners-images'
 import Image from 'next/image'
 import Link from 'next/link'
 import styles from './banner.module.scss';
+import { useEffect, useState } from 'react';
 
-export default function Banner() { 
+export default function Banner() {
+  const [isMobile, setIsMobile] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    }
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  const bannerImages = isMobile ? bannerImagesMobile : bannerImagesLarge;
   return (
     <section className={styles['banner-wrapper']}>
-        <Carousel
-          dots={false}
-          infinite={true}
-          speed={2000}
-          autoplay
-          autoplaySpeed={10000}
-          effect="fade"
-        >
-          {bannerImagesLarge.map((banner, index) => (
-            <Link
-              key={index}
-              href="/category/trends"
-              className={styles['image-link']}
-            >
-              <div className={styles['image-wrapper']}>
-                <Image
-                  src={banner}
-                  alt="banner"
-                  width={1800}
-                  height={900}
-                  layout='responsive'
-                  priority={index === 0}
-                />
-              </div>
-            </Link>
-          ))}
-        </Carousel>
-      </section>
+      <Carousel
+        dots={false}
+        infinite={true}
+        speed={2000}
+        autoplay
+        autoplaySpeed={10000}
+        effect="fade"
+      >
+        {bannerImages.map((banner, index) => (
+          <Link
+            key={index}
+            href="/category/trends"
+            className={styles['image-link']}
+          >
+            <div className={styles['image-wrapper']}>
+              <Image
+                src={banner}
+                alt="banner"
+                width={1800}
+                height={900}
+                layout='responsive'
+                priority={index === 0}
+              />
+            </div>
+          </Link>
+        ))}
+      </Carousel>
+    </section>
   )
 };
--- frontend/src/constants/banners-images.ts
+++ frontend/src/constants/banners-images.ts
@@ -3,7 +3,7 @@ export const bannerImagesLarge = [
   '/banners/banner-large-screen-2.webp'
 ];
 
-export const bannerImagesSmall = [
-  '/banner-mobile-screen-1.jpg',
-  '/banner-mobile-screen-1.jpg'
+export const bannerImagesMobile = [
+  '/banners/banner-mobile-screen-1.webp',
+  '/banners/banner-mobile-screen-1.webp'
 ];
