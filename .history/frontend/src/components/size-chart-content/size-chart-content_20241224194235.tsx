27dc0440549f757a49a38cac95a601e0917bb721
Lenar <e6naa7981rrshift934a@gmail.com>
2024-12-24 19:43:13 +0400

    fix: size chart content width modal

---
+++ .history/frontend/src/components/size-chart-content/size-chart-content_20241224194235.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import CloseButton from "../ui/close-button/close-button";
+import styles from './size-chart-content.module.scss';
+
+export default function SizeChartContent(
+  { onClose }: { onClose: () => void }) {
+  const sizesImage =
+    'https://cdn.kiwisizing.com/simpleretro-1610555643896.png';
+  return (
+    <div className={styles['size-chart-content']}>
+      <div className={styles['text-wrapper']}>
+        <CloseButton onClose={onClose} />
+      </div>
+      <div className={styles.subtitle}>Size Charts</div>
+
+      <Image
+        src={sizesImage}
+        alt="Choose sizes"
+        width={0}
+        height={0}
+        layout="responsive"
+      />
+    </div>
+  )
+};
--- frontend/src/components/size-chart-content/size-chart-content.tsx
+++ frontend/src/components/size-chart-content/size-chart-content.tsx
@@ -4,22 +4,21 @@ import styles from './size-chart-content.module.scss';
 
 export default function SizeChartContent(
   { onClose }: { onClose: () => void }) {
-  const sizesImage = 
-  'https://cdn.kiwisizing.com/simpleretro-1610555643896.png';
+  const sizesImage =
+    'https://cdn.kiwisizing.com/simpleretro-1610555643896.png';
   return (
-      <div className={styles['size-chart-content']}>
-        <div className={styles['text-wrapper']}>
-          <CloseButton onClose={onClose} />
-        </div>
-        <div className={styles.subtitle}>Size Charts</div>
-        <div />
-        <Image
-          src={sizesImage}
-          alt="Choose sizes"
-          width={0}
-          height={0}
-          layout="responsive"
-        />
+    <div className={styles['size-chart-content']}>
+      <div className={styles['text-wrapper']}>
+        <CloseButton onClose={onClose} />
       </div>
+      <div className={styles.subtitle}>Size Charts</div>
+      <Image
+        src={sizesImage}
+        alt="Choose sizes"
+        width={0}
+        height={0}
+        layout="responsive"
+      />
+    </div>
   )
 };
