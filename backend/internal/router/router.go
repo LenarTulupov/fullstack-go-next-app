5e06677756b23f72c30655b68d944d8668540890
Lenar Tulupov <e6naa7981rrshift934a@gmail.com>
2025-08-25 15:25:19 +0400

    fix: added consts for review service and handler in router

--- backend/internal/router/router.go
+++ backend/internal/router/router.go
@@ -1,14 +1,15 @@
 package router
 
 import (
-    "github.com/gin-gonic/gin"
-    "api/pkg/handlers"
-    "api/pkg/services"
-    "api/pkg/repository"
-    "api/internal/middleware"
     "database/sql"
     "net/http"
+
+    "api/internal/middleware"
     "api/pkg/config"
+    "api/pkg/handlers"
+    "api/pkg/repository"
+    "api/pkg/services"
+    "github.com/gin-gonic/gin"
 )
 
 func SetupRouter(db *sql.DB) *gin.Engine {
@@ -22,15 +23,19 @@ func SetupRouter(db *sql.DB) *gin.Engine {
     productService := services.NewProductService(productRepo)
     productHandler := handlers.NewProductHandler(productService)
 
-    // Admin 
+    // Initialize the review service and repository
+    reviewRepo := repository.NewReviewRepository(db)
+    reviewService := services.NewReviewService(reviewRepo)
+    reviewHandler := handlers.NewReviewHandler(reviewService)
+
+    // Admin routes
     r.POST("/admin/login", handlers.AdminLogin)
 
     admin := r.Group("/admin/dashboard")
-
     admin.Use(middleware.AuthMiddleware("admin"))
     admin.GET("", handlers.AdminDashboard)
 
-
+    // User routes
     r.POST("/register", handlers.RegisterUser)
     r.POST("/login", handlers.LoginUser)
     r.GET("/users/:id", handlers.GetUser)
@@ -39,6 +44,7 @@ func SetupRouter(db *sql.DB) *gin.Engine {
     r.PUT("/users/:id", handlers.UpdateUser)
     r.DELETE("/users/:id", handlers.DeleteUser)
 
+    // Health check
     r.GET("/health", handlers.HealthHandler)
 
     // Product routes
@@ -46,9 +52,11 @@ func SetupRouter(db *sql.DB) *gin.Engine {
     r.GET("/products/id/:id", productHandler.GetProduct)
     r.GET("/products/:slug", productHandler.GetProductBySlug)
 
+    // Review routes
     r.GET("/products/:productID/reviews", reviewHandler.GetReviews)
     r.POST("/products/:productID/reviews", reviewHandler.AddReview)
 
+    // Debug route
     r.GET("/debug/jwt-secret", func(c *gin.Context) {
         c.JSON(http.StatusOK, gin.H{"JWT_SECRET_KEY": config.JwtSecretKey})
     })
